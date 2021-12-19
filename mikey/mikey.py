38fac423802348b13093ad4e70902996cedab83e
Ryan Shah <ryan91396@gmail.com>
2021-12-19 14:36:19 -0600

    added dad joke response

--- mikey/mikey.py
+++ mikey/mikey.py
@@ -13,5 +13,23 @@ async def on_ready():
     print("We have logged in as {0}".format(mikey.user.name))
 
 
+@mikey.event
+async def on_message(message):
+    search = "i'm "
+    if search in message.content.lower():
+        msg = message.content
+        start = msg.lower().index(search) + 4
+        end = len(msg)
+        if '.' in msg:
+            idx = msg.index('.')
+            if idx > start:
+                end = idx
+        if ',' in msg:
+            idx = msg.index('.')
+            if idx > start and idx < end:
+                end = idx
+        response = "Hi " + msg[start:end] + ", I thought you were <@" + str(message.author.id) + ">."
+        await message.reply(response)
+
 mikey.add_cog(Music(mikey))
 mikey.run(DISCORD_API_TOKEN)

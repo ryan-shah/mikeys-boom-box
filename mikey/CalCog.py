39cb1e91797da6f19e45a4e5eddf867f296bea12
Ryan Shah <ryan91396@gmail.com>
2023-05-14 22:14:31 -0500

    debugging around oauth flow

--- mikey/CalCog.py
+++ mikey/CalCog.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from GoogleCalendar import GCalendar, load_calendars
+from exceptions import AuthenticationException
 
 class Calendar(commands.Cog):
 
@@ -8,29 +9,34 @@ class Calendar(commands.Cog):
         load_calendars()
         self.calendars = {}
 
-    def get_calendar(self, guild_id):
+    async def get_calendar(self, guild_id, ctx=None):
         """Retrieves the calendar for the guild, or generates a new one"""
         try:
             calendar = self.calendars[guild_id]
         except KeyError:
-            calendar = GCalendar(guild_id)
-            self.calendars[guild_id] = calendar
-        
+            try:
+                calendar = GCalendar(guild_id)
+                self.calendars[guild_id] = calendar
+            except AuthenticationException as e:
+                if ctx:
+                    await ctx.send(str(e))
+                return
+                       
         return calendar
 
     @commands.Cog.listener()
     async def on_scheduled_event_create(self, event):
-        calendar = self.get_calendar(event.guild.id)
+        calendar = await self.get_calendar(event.guild.id)
         calendar.publish_event(event)
         
     @commands.Cog.listener()
     async def on_scheduled_event_update(self, before, after):
-        calendar = self.get_calendar(before.guild.id)
+        calendar = await self.get_calendar(before.guild.id)
         calendar.modify_event(before, after)
 
     @commands.Cog.listener()
     async def on_scheduled_event_delete(self, event):
-        calendar = self.get_calendar(event.guild.id)
+        calendar = await self.get_calendar(event.guild.id)
         calendar.delete_event(event)
 
     @commands.command(name="register", aliases=["reg", "r"])
@@ -41,7 +47,7 @@ class Calendar(commands.Cog):
         calendarId: string [Required]
             The ID of the calendar you wish to use.
         """
-        calendar = self.get_calendar(ctx.guild.id)
+        calendar = await self.get_calendar(ctx.guild.id, ctx)
         calendar.register_calendar(url)
         print(calendar.get_calendars())
         
@@ -52,7 +58,7 @@ class Calendar(commands.Cog):
         ------------
         none
         """
-        calendar = self.get_calendar(ctx.guild.id)
+        calendar = await self.get_calendar(ctx.guild.id, ctx)
         msg = "**Synced Calnedars**\n"
         for c in calendar.list_calendars():
             msg += f"{c['name']}: <{c['url']}>\n"
@@ -65,7 +71,7 @@ class Calendar(commands.Cog):
         ------------
         none
         """
-        calendar = self.get_calendar(ctx.guild.id)
+        calendar = await self.get_calendar(ctx.guild.id, ctx)
         calendar.create_calendar(ctx.guild.name)
         await ctx.send('Calendar Created')
         await ctx.invoke(self.list_)
@@ -77,7 +83,7 @@ class Calendar(commands.Cog):
         ------------
         none
         """
-        calendar = self.get_calendar(ctx.guild.id)
+        calendar = await self.get_calendar(ctx.guild.id, ctx)
         c = calendar.remove_calendar(name)
         if c:
             await ctx.send(f"Calendar Removed: {c['name']}")
--- mikey/GoogleCalendar.py
+++ mikey/GoogleCalendar.py
@@ -1,8 +1,10 @@
 from constants import calendarFile
+from exceptions import AuthenticationException
 from datetime import datetime, timedelta
 import base64
 import csv
 import os.path
+import inspect
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -91,6 +93,14 @@ class GCalendar():
 
     def get_calendars(self):
         return self.calendars
+    
+    def get_new_creds(self):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        try:
+            return flow.run_local_server(port=0, open_browser=False, timeout_seconds=30)
+        except Exception as e:
+            raise AuthenticationException("Google", e, inspect.stack()[0][3])
 
     def get_creds(self):
         creds = None
@@ -102,11 +112,12 @@ class GCalendar():
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
+                try:
+                    creds.refresh(Request())
+                except Exception as e:
+                    creds = self.get_new_creds()
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    'credentials.json', SCOPES)
-                creds = flow.run_local_server(port=0)
+                creds = self.get_new_creds()
             # Save the credentials for the next run
             with open('token.json', 'w') as token:
                 token.write(creds.to_json())
